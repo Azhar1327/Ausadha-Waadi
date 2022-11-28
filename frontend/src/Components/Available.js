809f31209280c448127eadbee8c5b5fec75312c9
Abhishek <abhishekkumar8162@gmail.com>
2022-11-29 01:37:07 +0530

    Available.js added to Homepage

--- frontend/src/Components/Available.js
+++ frontend/src/Components/Available.js
@@ -1,11 +1,56 @@
 import React from 'react'
+import {useRef, useEffect} from 'react';
+import '../CSS/Available.css'
+import {useState} from 'react';
+
+
 
 function Available() {
-  return (
+    
+
+    const [pin, setPin] = useState('');
+
+  const handleSubmit = event => {
+    console.log('handleSubmit ran');
+    event.preventDefault(); 
+    setPin('');
+  }
+
+      var zipcodes = [443404, 411045, 754142];
+
+var btn = document.getElementById('submit-btn');
+
+ function checkAvailability() {
+ 	var zipcode = pin;
+ 	var res = document.getElementById("res");
+	
+ 	if(zipcodes.includes(parseInt(zipcode))) {
+		console.log('true');
+		res.textContent = "Available"
+		 res.classList.add("available");
+		 res.classList.remove("not-available");
+		setPin('');
+	} 
+    else {
+		console.log("false");
+		res.textContent = "Not Available"
+		res.classList.add("not-available");
+		res.classList.remove("available");
+		setPin('');
+	}
+}
+
+ 
+  
+
+    return (
     <>
-    
-    Available
-    
+    <div className="Availability">
+    <input id="zipcode" type="text"  onChange={event => setPin(event.target.value)}
+          value={pin} placeholder="Enter the zipcode"/>
+	<button  onClick={checkAvailability} id="submit-btn" type="submit" >Check</button>
+	<div id="res"></div>
+    </div>
     </>
   )
 }
--- frontend/src/Components/Homepage.js
+++ frontend/src/Components/Homepage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Available from "./Available.js";
 import "../CSS/Homepage.css";
 function Homepage() {
   return (
     <>
+
       <div className="Services">
         <div className="card">
           <img
@@ -56,6 +58,180 @@ function Homepage() {
           </div>
         </div>
       </div>
+
+
+
+    <h2 className="Services-heading">Medicines</h2>
+
+
+    <div className="Services">
+
+    
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Medicines</b></h4> 
+    <button>Book it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Lab Tests</b></h4> 
+    <button>Schedule it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Health Blogs</b></h4> 
+    <button>Explore it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Health Blogs</b></h4> 
+    <button>Explore it</button> 
+    </div>
+    </div>
+
+    </div>
+
+    <div className="Services">
+
+    
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Medicines</b></h4> 
+    <button>Book it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Lab Tests</b></h4> 
+    <button>Schedule it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Health Blogs</b></h4> 
+    <button>Explore it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Health Blogs</b></h4> 
+    <button>Explore it</button> 
+    </div>
+    </div>
+
+    </div>
+
+ 
+
+    <h2 className="Services-heading">Lab Tests</h2>
+
+
+    <div className="Services">
+
+    
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Medicines</b></h4> 
+    <button>Book it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Lab Tests</b></h4> 
+    <button>Schedule it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Health Blogs</b></h4> 
+    <button>Explore it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0" />
+    <div className="container">
+    <h4><b>Health Blogs</b></h4> 
+    <button>Explore it</button> 
+    </div>
+    </div>
+
+    </div>
+
+    <div className="Services">
+
+    
+
+    
+    <div className="Services">
+
+    
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Medicines</b></h4> 
+    <button>Book it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Lab Tests</b></h4> 
+    <button>Schedule it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0" alt="Avatar" />
+    <div className="container">
+    <h4><b>Health Blogs</b></h4> 
+    <button>Explore it</button> 
+    </div>
+    </div>
+
+    <div className="card">
+    <img src="https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0" />
+    <div className="container">
+    <h4><b>Health Blogs</b></h4> 
+    <button>Explore it</button> 
+    </div>
+    </div>
+
+    </div>
+
+    </div>
+    
+    <h2 className="Services-heading">Search the Centre</h2>
+    <Available/>
+
+
     </>
   );
 }
