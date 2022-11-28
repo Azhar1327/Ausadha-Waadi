d66f9310fa2620803f59090d9305049163c8d24b
AMIT ROUT <amitroutard@gmail.com>
2022-11-28 23:37:40 +0530

    Ui changes and Backend changes
    Made changes in React, Deleted useless components and add medicine search option in backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,27 @@
+import Header from "./Components/Header.js";
+import Homepage from "./Components/Homepage.js";
 
-import './App.css';
-import Header from './Components/Header.js';
-import Homepage from './Components/Homepage.js';
-
-import { BrowserRouter as Router, Routes, Route, 
-  Navigate,Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+} from "react-router-dom";
 
 import React from "react";
 
 function App() {
   return (
+    <>
+      <Header />
 
-   <>
-
-<Header/>
-
-
-    <Router>
-
-  <Routes>
-    <Route path="/" element={<Homepage/>}/>
-
-    </Routes>
-    </Router> 
-
-
-
-   </>
-    
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
